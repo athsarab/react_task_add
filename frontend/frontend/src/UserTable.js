bcf9848842e55c48c031d899e84beb352decacea
athsara bimalka <athsarab@gmail.com>
2025-02-27 02:04:09 +0530

    table

---
+++ frontend/frontend/src/UserTable.js
@@ -0,0 +1,37 @@
+import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+
+const UserTable = ({ users }) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {users.length > 0 ? (
+                        users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.name}</TableCell>
+                                <TableCell>
+                                    <button style={{ margin: '0px 10px' }} onClick={() => {}}>Edit</button>
+                                    <button onClick={() => {}}>Delete</button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={3}>No users</TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default UserTable;
--- frontend/frontend/src/Useres.js
+++ frontend/frontend/src/Useres.js
@@ -1,9 +1,37 @@
+import { Box } from "@mui/material";
 import UserForm from "./UserForm";
+import UserTable from "./UserTable";
+
+
+const users = [
+    {
+        id: 1,
+        name: "John Doe"
+    },
+    {
+        id: 2,
+        name: "Jane Doe"
+    }
+];
+
 
 const Users = () => {
     return (
-
+<Box 
+sx = {
+    {
+        width :'calc(100% - 240px)',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '50px',
+ 
+    }
+}
+>
         <UserForm />
+        <UserTable users={users} />
+
+</Box>
 
     )
 }
