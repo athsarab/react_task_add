f1e013efe4d9bdf940449dd2fe36d79a5b54ae04
athsara bimalka <athsarab@gmail.com>
2025-03-05 02:07:30 +0530

    controller

--- server/controller.js
+++ server/controller.js
@@ -1,24 +1,47 @@
-const  users =[
-    {
-        id: 1,
-        name: 'John Doe',
-        
-    },
-    {
-        id: 2,
-        name: ' Doe',
-        
-    },
-];
+const User = require('./models');
 
-const getUsers = (cb) =>{
-    cb(users);
+
+const getUsers = (req,res,next) => {
+    User.find().then(response => {
+        res.json(response);
+    }).catch(err => {
+        res.json(err);
+    });
 }
 
-const getUserByID = (id, cb) =>{
-    const user = users.find(user => user.id == id);
-    cb(user);
+const addUser = (req,res,next) => {
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name,
+    });
+    user.save().then(response => {
+        res.json(response);
+    }).catch(err => {
+        res.json(err);
+    });
 }
+
+const updateUser = (req,res,next) => {
+    const {id, name} = req.body;
+    User.updateOne({id: id}, {name: name}).then(response => {
+        res.json(response);
+    }).catch(err => {
+        res.json(err);
+    });
+}
+
+const deleteUser = (req,res,next) => {
+    const id = req.body.id;
+    User.deleteOne({id: id}).then(response => {
+        res.json(response);
+    }).catch(err => {
+        res.json(err);
+    });
+}
+
+    
 
 exports.getUsers = getUsers;
-exports.getUserByID = getUserByID;
+exports.addUser = addUser;
+exports.updateUser = updateUser;
+exports.deleteUser = deleteUser;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 3001;
 const host = 'localhost';
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+const uri =  'mongodb+srv://athsarab:athsarab@cluster2.4qpsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2'
+
+const connect = async () => {
+    try{
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('Connected to MongoDB');
+
+    }
+    catch (error){
+        console.log('Error connecting to MongoDB');
+    }
+}
+
+connect();
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
