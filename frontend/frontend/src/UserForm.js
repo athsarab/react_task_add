5de6b3b0f7fb992d07715351b1c05b7c2b545b1b
athsara bimalka <athsarab@gmail.com>
2025-03-20 10:25:57 +0530

    form

--- frontend/frontend/src/UserForm.js
+++ frontend/frontend/src/UserForm.js
@@ -97,7 +97,7 @@ const UserForm = ({addUser}) => {
                             
 
                         }}
-
+  
                         onClick={() => {
                             addUser({id: id, name: name});
  
